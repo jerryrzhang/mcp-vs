473a9e378b0d2f19f96fa333122bf841f1eda9a2
Jerry Zhang <69628481+jerryrzhang@users.noreply.github.com>
2026-02-23 23:51:16 +0800

    prac2

--- src/Robot.c
+++ src/Robot.c
@@ -10,28 +10,53 @@
 int main(void)
 {
   DDRA = 0; //put PORTA into input mode
-  DDRA |= (1<<PA3);
   PORTA = 0;
   PORTA |= (1<<PA7); // enable internal pullup PA7
 
+  DDRC = 0xFF; //portc into output
+  PORTC = 0; //clear leds
+
+  bool horizontal = true;
+  bool pressed = false;
   adc_init();
   _delay_ms(20);
 
   uint16_t adcVal = 0;
-
   while(1)
   {
-    if (!(PINA & (1<<PA7))) //PA7 low / button pressed
-    {
-      PORTA |= (1<<PA3);
+    if (PINA & (1<<PA2)) {
+      if (~pressed){
+        horizontal = ~horizontal;
+        pressed = true;
+      }
+    } else {
+      if (pressed) {
+        pressed = false;
+      }
     }
-    else
-    {
-      PORTA &= ~(1<<PA3);
-    }
-    adcVal = adc_read(3); //returns a 10bit value on voltage at ADC3
 
 
+    if (horizontal) {
+      adcVal = adc_read(3); //returns a 10bit value on voltage at ADC3 from 0 - 1023
+    } else {
+      adcVal = adc_read(4);
+    }
+
+    //  changing this into 8 leds we get
+    // 0-127-255-383-511-639-767-895-1023
+    adcVal = adcVal / 128; //divides and truncates decimal
+    uint8_t lookup[] = {
+      0b11110000,
+      0b01110000,
+      0b00110000,
+      0b00010000,
+      0b00001000,
+      0b00001100,
+      0b00001110,
+      0b00001111
+    };
+
+    PORTC = lookup[adcVal];
   }
  return(1);
 }//end main 
