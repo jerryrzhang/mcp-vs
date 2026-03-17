e6194eea848da925cea038fc10101038dee9fa85
Jerry Zhang <69628481+jerryrzhang@users.noreply.github.com>
2026-03-17 16:11:54 +0800

    Update Robot.c

--- src/Robot.c
+++ src/Robot.c
@@ -5,18 +5,26 @@
 //include this .c file's header file
 #include "Controller.h"
 
-//static function prototypes, functions only called in this file
 
-volatile uint32_t value = 0;
-uint8_t falling = 0;
-uint32_t last_time = 0;
-char serialString[60] = {};
-uint32_t last_time_interrupt = 0;
+int milliseconds1 = 0;
+int seconds1 = 0;
+int minutes1 = 0;
+bool stopped = false;
+int debounce = 0;
+bool ignore = false;
+
+bool stopped2 = false;
+int debounce2 = 0;
+bool ignore2 = false;
+
+char serialString[60];
+
+//static function prototypes, functions only called in this file
 
 int main(void)
 {
-  DDRA = 0xFF;
-  PORTA = 0;
+  DDRD = 0;
+  PORTD = (1 << PD2) | (1 << PD3);
 
   adc_init();
   _delay_ms(20);
@@ -25,98 +33,29 @@ int main(void)
 
   serial0_init();
 
+  TCCR1A = 0;
+
+  TCCR1B = (1<<CS12) | (1<<3); // 00000100 prescaler of 256 
+   
+
+
+  TIMSK1 = (1<<OCIE1A);
+
+  OCR1A = 624; //compare register
   
+  EICRA &= ~(1<<ISC21);
+  EICRA |= (1<<ISC20);
+  EIMSK |= (1<<INT2);
+
+  EIMSK |= (1<<INT3);
+  EICRA &= ~(1<<ISC31);
+  EICRA |= (1<<ISC30);
+
+
+  sei();
+
   while(1) {
-    // PORTA |= (1);
-    // _delay_ms(1000);
 
-    // PORTA %= (1);
-    // _delay_ms(1000);
-
-    uint8_t adc_raw = adc_read(0)/8;
-    uint16_t distance_lut[] = {
-      2048, //8
-      1024, //16
-      682, //24
-      512, //32
-      410, //40
-      341, //48
-      292, //56
-      256, //64
-      228, //72
-      205, //80
-      186, //88
-      170, //96
-      157, //104
-      146, //112
-      137, //120
-      128, //128
-      121, //136
-      114, //144
-      108, //152
-      102, //160
-      97, //168
-      92, //176
-      88, //184
-      85, //192
-      81, //200
-      79, //208
-      76, //216
-      74, //224
-      71, //232
-      69, //240
-      67, //248
-      65, //256
-      63, //264
-      61, //272
-      60, //280
-      58, //288
-      57, //296
-      55, //304
-      54, //312
-      53, //320
-      52, //328
-      50,  //336
-      512, //344
-      410, //352
-      341, //360
-      292, //368
-      256, //376
-      228, //384
-      205, //392
-      186, //400
-      170, //408
-      157, //416
-      146, //424
-      137, //432
-      128, //440
-      121, //448
-      114, //456
-      108, //464
-      102, //472
-      97, //480
-      92, //488
-      88, //496
-      85, //504
-      81  //512
-    };
-
-    adcVal = 3000/(adc_read(0)+20)-1.5;
-
-
-    if (adcVal > 50) {
-      adcVal = 50;
-    }
-
-    sprintf(serialString, "\n Distance? %ucm", adcVal);
-    serial0_print_string(serialString);
-    _delay_ms(100);
-
-    // if(adcVal > 250) {
-    //   PORTA |= 1;
-    // } else {
-    //   PORTA &= ~(1);
-    // }
   }
   
 
@@ -125,9 +64,48 @@ int main(void)
 }//end main
 
 
+ISR(TIMER1_COMPA_vect)
+{
+  debounce -= 1;
+  debounce2 -= 1;
+  if (!stopped) {
+    milliseconds1 += 10;
+  }
+  if (milliseconds1 == 1000) {
+    milliseconds1 = 0;
+    seconds1 += 1;
+  }
+  if (seconds1 == 60) {
+    seconds1 = 0;
+    minutes1 += 1;
+  }
+  sprintf(serialString, "Time: %02d:%02d:%03d\n", minutes1, seconds1, milliseconds1);
+  serial0_print_string(serialString);
+  return;
+}
+
 ISR(INT2_vect) {
-  //if ((milliseconds_now()-last_time_interrupt) > 20) {
-  //  last_time_interrupt = milliseconds_now();
-    falling += 1;
-  //}
+  if (debounce <= 0) {
+    if (!ignore) {
+      stopped = !stopped;
+      ignore = true;
+    } else {
+      ignore = false;
+    }
+    debounce = 3;
+  }
+}
+
+ISR(INT3_vect) {
+  if (debounce2 <= 0) {
+    if (!ignore2) {
+      milliseconds1 = 0;
+      seconds1 = 0;
+      minutes1 = 0;
+      ignore2 = true;
+    } else {
+      ignore2 = false;
+    }
+    debounce2 = 3;
+  }
 }
